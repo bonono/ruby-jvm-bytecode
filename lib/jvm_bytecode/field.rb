c0b364eba9e1a14ad19e28ca70d15e3f56055f45
bonono <bonono.jp@gmail.com>
2017-01-06 18:21:06 +0900

    add comment to Field

--- lib/jvm_bytecode/field.rb
+++ lib/jvm_bytecode/field.rb
@@ -1,4 +1,6 @@
 module JvmBytecode
+  # Implementation for "field_info" structure
+  # @see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html#jvms-4.5
   class Field
     using Extensions
     include AccessFlag
@@ -16,6 +18,11 @@ module JvmBytecode
       enum: 0x4000
     }.freeze 
 
+    # Decode concatenated "field_count" and "fields"
+    #
+    # @param cp [JvmBytecode::ConstantPool]
+    # @param io [IO]
+    # @return [Array<JvmBytecode::Field>]
     def self.decode_serial(cp, io)
       Array.new(io.read(2).unpack('S>').first) do 
         new(cp).tap { |f| f.decode(io) }
@@ -30,26 +37,41 @@ module JvmBytecode
       @descriptor = 0
     end
 
+    # Setter and getter for name_index
+    #
+    # @param n [Integer, String]
+    # @return [Integer]
     def name(n = nil)
       @name = cp.index_or_utf8(n) if n
       @name
     end
 
+    # Setter and getter for descriptor_index
+    #
+    # @param d [Integer, String]
+    # @return [Integer]
     def descriptor(d = nil)
       @descriptor = cp.index_or_utf8(d) if d
       @descriptor
     end
 
+    # Return bytecode
+    #
+    # @return [String] ASCII-8BIT string
     def bytecode
       [access_flag, @name, @descriptor].pack('S>3') + attributes.join_bytecodes
     end
 
+    # Decode bytecode
+    #
+    # @param io [IO]
     def decode(io)
       acc_flag, @name, @descriptor = io.read(6).unpack('S>3')
       set_access_flag(acc_flag)
       set_attributes(Attributes::Attribute.decode_serial(cp, io))
     end
 
+    # @return [Hash]
     def to_hash
       {
         access_flag: readable_access_flag,
